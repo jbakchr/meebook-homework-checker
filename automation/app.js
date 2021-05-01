0a26f03b2ad0231acf35a785a0f8af557a2a9ec8
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-01 22:48:51 +0200

    Initial automation setup done

---
+++ automation/app.js
@@ -0,0 +1,9 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://meebook.com/");
+
+  await browser.close();
+})();
