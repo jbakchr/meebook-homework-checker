6eb0ec95805e615ce02e8c28ceae894a601ff887
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-02 11:24:32 +0200

    Added submission of username

--- automation/app.js
+++ automation/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const puppeteer = require("puppeteer");
 
 (async () => {
@@ -13,6 +14,13 @@ const puppeteer = require("puppeteer");
     "body > div.portal-frontpage > div > div.portal-frontpage__main-content > div > div:nth-child(2) > div > div:nth-child(1) > div > a > span";
   await page.waitForSelector(btnSelector);
   await page.click(btnSelector);
+
+  // Submit username
+  const usernameSelector = "#username";
+  const submitSelector = "body > main > div > div > form > nav > button";
+  await page.waitForTimeout(2000);
+  await page.type(usernameSelector, process.env.USERNAME, { delay: 250 });
+  await page.click(submitSelector);
 
   // await browser.close();
 })();
