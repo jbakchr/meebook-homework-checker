32e6ec7da933f9c684801c39df46fe73f3f36001
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-02 10:39:51 +0200

    Added initial click to begin login process

--- automation/app.js
+++ automation/app.js
@@ -1,9 +1,18 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  // Go to login page
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
-  await page.goto("https://meebook.com/");
+  await page.goto("https://app.meebook.com/foraeldre/1");
 
-  await browser.close();
+  // Click login button
+  const btnSelector =
+    "body > div.portal-frontpage > div > div.portal-frontpage__main-content > div > div:nth-child(2) > div > div:nth-child(1) > div > a > span";
+  await page.waitForSelector(btnSelector);
+  await page.click(btnSelector);
+
+  // await browser.close();
 })();
