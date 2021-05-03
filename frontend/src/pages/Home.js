675ebdd6876f4cdf837ae7bd8eab3e7ea61edfa8
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-03 10:45:39 +0200

    Added initial routing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "@material-ui/core";
 
 const Home = () => {
-  return <Container>Hello world</Container>;
+  return <Container>Hello from the Home page</Container>;
 };
 
 export default Home;
