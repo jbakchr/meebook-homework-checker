b17eddb22d92e564d5a19df892a363611a90e1d6
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-03 22:32:19 +0200

    Added initial db setup

---
+++ backend/db/db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize");
+
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  dialect: "mysql",
+});
+
+module.exports = db;
