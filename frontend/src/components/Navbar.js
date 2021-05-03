47d24a2bba940be96f2db473a327b5b7ca0686cd
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-03 10:54:39 +0200

    Added initial login page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   AppBar,
   Toolbar,
@@ -14,9 +15,18 @@ const Navbar = () => {
         <Toolbar>
           <Container style={{ display: "flex" }}>
             <Typography style={{ flexGrow: 1 }} variant="h6">
-              Meebook Homework Checker
+              <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                Meebook Homework Checker
+              </Link>
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button color="inherit">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/login"
+              >
+                Login
+              </Link>
+            </Button>
           </Container>
         </Toolbar>
       </AppBar>
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const Login = () => {
+  return <Container>Login page ..</Container>;
+};
+
+export default Login;
