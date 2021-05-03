617882b6439d71a2594e9a7aa071b8bdb6af5c2d
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-03 10:41:27 +0200

    Added initial Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      Meebook Homework Checker
+      <Home />
     </div>
   );
 };
