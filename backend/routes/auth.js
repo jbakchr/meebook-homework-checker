2dd343ae0bb2589dd9f8effd409f0a0e5ee2e4c5
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-05-03 21:11:07 +0200

    Added initial login route

---
+++ backend/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { check, validationResult } = require("express-validator");
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  (req, res, next) => {
+    // Check for errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Get email and password
+    const { email, password } = req.body;
+
+    try {
+      res.json({ email, password });
+    } catch (err) {
+      res.status(500).json({ msg: "Server error" });
+    }
+  }
+);
+
+module.exports = router;
